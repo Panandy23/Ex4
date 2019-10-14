650401b07875007d64d074a636a58d38b82d907d
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-14 12:21:33 +0200

    program poprawnie sprawdza długości boków trójkąta

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int a, b, c;
 
+        Scanner scaner = new Scanner(System.in);
+
+        System.out.println("Podaj długość boku a: ");
+        a = scaner.nextInt();
+        System.out.println("Podaj długość boku b: ");
+        b = scaner.nextInt();
+        System.out.println("Podaj długość boku c: ");
+        c = scaner.nextInt();
+
+
+        if ((a*a + b*b) == c*c){
+            System.out.println("Z tych boków można zbudować trójkąt prostokątny");
+
+        } else{
+            System.out.println("Z tych boków trójkąta prostokątnego nie zbudujesz");
+        }
 
 
 
